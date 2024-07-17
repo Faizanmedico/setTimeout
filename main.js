155b0280fcb223026be621b597b188d46106e930
Sultan Ahmed <146020547+Faizanmedico@users.noreply.github.com>
2024-07-17 12:40:45 +0500

    Add files via upload
    TypeScript type for setTimeout or setInterval timers

---
+++ main.js
@@ -0,0 +1,46 @@
+//       Countdown to a rocket launch
+setTimeout(function () {
+    console.log("Countdown to a rocket launch");
+}, 1000);
+setTimeout(function () {
+    console.log("Ready to Start");
+}, 2000);
+setTimeout(function () {
+    console.log("start");
+}, 4000);
+setTimeout(function () {
+    console.log("9");
+}, 6000);
+setTimeout(function () {
+    console.log("8");
+}, 7000);
+setTimeout(function () {
+    console.log("7");
+}, 8000);
+setTimeout(function () {
+    console.log("6");
+}, 9000);
+setTimeout(function () {
+    console.log("5");
+}, 10000);
+setTimeout(function () {
+    console.log("4");
+}, 11000);
+setTimeout(function () {
+    console.log("3");
+}, 12000);
+setTimeout(function () {
+    console.log("2");
+}, 13000);
+setTimeout(function () {
+    console.log("1");
+}, 14000);
+setTimeout(function () {
+    console.log("0");
+}, 15000);
+setTimeout(function () {
+    console.log("up!");
+}, 16000);
+setTimeout(function () {
+    console.log("Congratulations on the Successful Launch of the Space Rocket Mission!");
+}, 22000);
---
+++ main.ts
@@ -0,0 +1,96 @@
+
+
+
+//       Countdown to a rocket launch
+
+setTimeout(() => {
+    console.log("Countdown to a rocket launch");
+    
+}, 1000);
+
+setTimeout(() => {
+    console.log("Ready to Start");
+    
+}, 2000);
+
+setTimeout(() => {
+    console.log("start");
+    
+}, 4000);
+
+
+setTimeout(() => {
+    console.log("9");
+    
+}, 6000);
+
+
+setTimeout(() => {
+    console.log("8");
+    
+}, 7000);
+
+
+setTimeout(() => {
+    console.log("7");
+    
+}, 8000);
+
+
+setTimeout(() => {
+    console.log("6");
+    
+}, 9000);
+
+
+setTimeout(() => {
+    console.log("5");
+    
+}, 10000);
+
+
+setTimeout(() => {
+    console.log("4");
+    
+}, 11000);
+
+
+setTimeout(() => {
+    console.log("3");
+    
+}, 12000);
+
+
+setTimeout(() => {
+    console.log("2");
+    
+}, 13000);
+
+
+setTimeout(() => {
+    console.log("1");
+    
+}, 14000);
+
+
+
+
+setTimeout(() => {
+    console.log("0");
+    
+}, 15000);
+
+
+
+
+setTimeout(() => {
+    console.log("up!");
+    
+}, 16000);
+
+
+
+setTimeout(() => {
+    console.log("Congratulations on the Successful Launch of the Space Rocket Mission!");
+    
+}, 22000);
